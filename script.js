5f6db24bdf0927ffde7a92050d1172b25fb5a92e
ayayam <115098984+ayayam@users.noreply.github.com>
2026-06-29 17:21:49 -0400

    added prompt asking for grid size

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 const container = document.querySelector(".container");
+const gridPromptButton = document.createElement("button");
+container.appendChild(gridPromptButton);
+const gridNumber = Number(prompt("What size grid would you like? (Pick 1 - 100)"));
 
 function createGrid() {
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
 
