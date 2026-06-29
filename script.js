172e2135483808507835027286329a8010d52ed4
ayayam <115098984+ayayam@users.noreply.github.com>
2026-06-29 16:11:05 -0400

    added 'mouseenter' event listener to squares to toggle between black and white

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ function createGrid() {
     for (let i = 0; i < 256; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        square.addEventListener("mouseenter", () => {
+            square.classList.toggle("filledSquare");
+        })        
+
         container.appendChild(square);
     }    
     
 }
 
 createGrid();
+
+
+
