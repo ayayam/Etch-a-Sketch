78bfa5e648e3e439b2adedb81f1ca4ebd372a7b3
ayayam <115098984+ayayam@users.noreply.github.com>
2026-06-30 13:38:14 -0400

    added dom fix to clear previous grid before new grid is created, fixed css to move gridMaker button to top of page (above grid)

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 
 const container = document.querySelector(".container");
 const gridPromptButton = document.createElement("button");
-container.appendChild(gridPromptButton);
+container.before(gridPromptButton);
 gridPromptButton.textContent = "Make a grid!";
 gridPromptButton.classList.add("gridMaker");
-let gridNumber = 0;
 
 gridPromptButton.addEventListener("click", () => {
-    gridNumber = Number(prompt("What size grid would you like? (Pick 1 - 100)"));
-    gridPromptButton.remove();
-    createGrid();
-    return gridNumber;
+    const gridNumber = Number(prompt("What size grid would you like? (1 - 100)"));
+    
+    if (gridNumber >= 1 && gridNumber <= 100) {
+        container.replaceChildren()
+        createGrid(gridNumber);
+    } else {
+        alert("Please pick a number between 1 and 100")
+    }
+    
 })
 
-function createGrid() {
+function createGrid(gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    let size = 700 / gridNumber;
 
-    for (let i = 0; i < gridNumber * gridNumber; i++) {
+    for (let i = 0; i < gridArea; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-
+        square.style.height = `${size}`;
+        square.style.weight = `${size}`;
         square.addEventListener("mouseenter", () => {
             square.classList.toggle("filledSquare");
         })        
 
         container.appendChild(square);
+
+        if (i % gridArea === 0) {
+            // go to new line
+        }
     }    
 }
 
